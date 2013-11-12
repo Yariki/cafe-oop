aab1e0bfbdfc8cb1354c6ddf82f2544adac2e683
yariki <iyariki.ya@gmail.com>
2013-11-13 01:29:14 +0200

    updated: - added some class into diagram; - added source for Person;

---
+++ Cafe/Person.cpp
@@ -0,0 +1,69 @@
+///////////////////////////////////////////////////////////
+//  Person.cpp
+//  Implementation of the Class Person
+//  Created on:      13-Nov-2013 1:27:52 AM
+//  Original author: Yariki
+///////////////////////////////////////////////////////////
+
+#include "Person.h"
+
+
+Person::~Person(){
+
+}
+
+
+
+
+
+void Person::Person(){
+
+}
+
+
+void Person::Person(std::string name, std::string surname, Specialization specialization){
+
+}
+
+
+void Person::Person(Person p){
+
+}
+
+
+void Person::Person(){
+
+}
+
+
+void Person::setName(std::string name){
+
+}
+
+
+const std::string Person::getName(){
+
+	return  NULL;
+}
+
+
+void Person::setSurname(std::string surname){
+
+}
+
+
+std::string Person::setSurname(){
+
+	return  NULL;
+}
+
+
+void Person::setSpecialization(Specialization sp){
+
+}
+
+
+Specialization Person::getSpecialization(){
+
+	return  NULL;
+}
---
+++ Cafe/Person.h
@@ -0,0 +1,36 @@
+///////////////////////////////////////////////////////////
+//  Person.h
+//  Implementation of the Class Person
+//  Created on:      13-Nov-2013 1:27:52 AM
+//  Original author: Yariki
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_78019771_352A_43a6_8498_311F639F29FE__INCLUDED_)
+#define EA_78019771_352A_43a6_8498_311F639F29FE__INCLUDED_
+
+#include "Specialization.h"
+
+class Person
+{
+
+public:
+	virtual ~Person();
+
+	void Person();
+	void Person(std::string name, std::string surname, Specialization specialization);
+	void Person(Person p);
+	void Person();
+	void setName(std::string name);
+	const std::string getName();
+	void setSurname(std::string surname);
+	std::string setSurname();
+	void setSpecialization(Specialization sp);
+	Specialization getSpecialization();
+
+private:
+	std::string name_;
+	Specialization specialization_;
+	std::string surname_;
+
+};
+#endif // !defined(EA_78019771_352A_43a6_8498_311F639F29FE__INCLUDED_)
