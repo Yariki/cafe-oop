d264513d6e5d1db792e27c2db903b69a20488d50
yariki <iyariki.ya@gmail.com>
2013-11-15 01:29:22 +0200

    updated: - added some classes into class diagram;

---
+++ Cafe/ICafeObject.h
@@ -0,0 +1,24 @@
+///////////////////////////////////////////////////////////
+//  ICafeObject.h
+//  Implementation of the Interface ICafeObject
+//  Created on:      15-Nov-2013 12:32:18 AM
+//  Original author: Yariki
+///////////////////////////////////////////////////////////
+
+#if !defined(EA_D7B5D180_167A_4456_8EB0_7CDFB431FA74__INCLUDED_)
+#define EA_D7B5D180_167A_4456_8EB0_7CDFB431FA74__INCLUDED_
+
+class ICafeObject
+{
+
+public:
+	ICafeObject() {
+
+	}
+
+	virtual ~ICafeObject() {
+
+	}
+
+};
+#endif // !defined(EA_D7B5D180_167A_4456_8EB0_7CDFB431FA74__INCLUDED_)
